0c4db2fa84ea9d432c39a62be3bfc3d7d318c07d
Hoolay <hoolay@free.fr>
2018-10-18 23:54:29 +0900

    Blog : récupération et affichage des données de la base

--- PHP/04.blog/index.php
+++ PHP/04.blog/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "php/pdo.php";
+
 // définition d'une page par défaut "home"
 $template = "home";
 
@@ -15,4 +17,32 @@ if (array_key_exists('page', $_GET)) {
     }
 }
 
+//////////////////////////////////////////////////////////////////////////
+
+$articles = fetchAll("SELECT id, title, category FROM articles");
+
+
+// ce long test moche ne sert qu'a vérifier que j'ai une url de type :
+// index.php?page=article&&id=XX
+if (array_key_exists('page', $_GET) &&      // vérification que page est présente
+    $_GET['page'] == 'article' &&           // vérification que page == article
+    array_key_exists('id', $_GET) &&        // vérification que id est présent
+    ctype_digit($_GET['id'])
+)               // vérification que id est bien un nombre
+{
+
+    // filtrage de l'id
+    $article_id = intval($_GET['id']);
+
+    // requête de récupération des infos de l'article
+    $sql = "SELECT id, author, title, content, category, creationDate 
+            FROM articles
+            WHERE id = ?
+    ";
+
+    $article = fetch($sql, [$article_id]);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
 include "pages/layout.phtml";
---
+++ PHP/04.blog/php/pdo.php
@@ -0,0 +1,25 @@
+<?php
+
+
+////////////////////            CONNEXION           ////////////////////////////////
+$pdo = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+
+// Afficher les erreurs PDO
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+
+////////////////////         FONCTION UTILES        ////////////////////////////////
+function fetchAll($sql, array $params = []) {
+    global $pdo;
+    $request = $pdo->prepare($sql);
+    $request->execute($params);
+    return $request->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function fetch($sql, array $params = []) {
+    global $pdo;
+    $request = $pdo->prepare($sql);
+    $request->execute($params);
+    return $request->fetch(PDO::FETCH_ASSOC);
+}
