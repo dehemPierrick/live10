5114199c6e778e69afdc4a0b2d18b2da4bc17905
Hoolay <hoolay@free.fr>
2018-11-05 20:19:50 +0900

    Restaurant - dumpSql - addMealModel

---
+++ PHP/06.restaurant/application/models/MealModel.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class MealModel {
+    function getMeals() {
+        $db = new Database();
+        return $db->query("SELECT name FROM meals");
+    }
+
+    function createMeal($name, $description, $photo, $quantityInStock, $buyPrice, $salePrice) {
+        $sql = "INSERT INTO meals (Name, Description, Photo, QuantityInStock, BuyPrice, SalePrice) VALUES (?,?,?,?,?,?)";
+        $db = new Database();
+        $db->executeSql($sql, [$name, $description, $photo, $quantityInStock, $buyPrice, $salePrice]);
+    }
+
+    function getMeal($meal_id) {
+        $db = new Database();
+        return $db->queryOne("SELECT name FROM meals WHERE id = ?", [$meal_id]);
+    }
+
+    function removeMeal($meal_id) {
+        $db = new Database();
+        $db->executeSql("DELETE FROM meals WHERE id=?", [$meal_id]);
+    }
+}
