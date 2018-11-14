734e7961fb54a91e311677cc92c8ae82863e3aff
Hoolay <hoolay@free.fr>
2018-11-14 23:09:59 +0900

    Restaurant - ajout des clés API stripe

--- PHP/06.restaurant/restaurant.sql
+++ PHP/06.restaurant/restaurant.sql
@@ -16,6 +16,38 @@
 /*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
 --
+-- Table structure for table `bookings`
+--
+
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bookings` (
+  `Id`          INT(11)         NOT NULL AUTO_INCREMENT,
+  `Customer_Id` INT(10)         NOT NULL,
+  `BookingDate` DATETIME        NOT NULL,
+  `Quantity`    INT(2) UNSIGNED NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `booking_customers_Id_fk` (`Customer_Id`)
+)
+  ENGINE = MyISAM
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookings`
+--
+
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings`
+  DISABLE KEYS */;
+INSERT INTO `bookings` VALUES (1, 1, '2018-11-08 11:45:00', 8), (2, 1, '2018-11-08 11:45:00', 8);
+/*!40000 ALTER TABLE `bookings`
+  ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `customers`
 --
 
@@ -37,6 +69,7 @@ CREATE TABLE `customers` (
   UNIQUE KEY `Email` (`Email`)
 )
   ENGINE = InnoDB
+  AUTO_INCREMENT = 7
   DEFAULT CHARSET = utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,6 +80,15 @@ CREATE TABLE `customers` (
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers`
   DISABLE KEYS */;
+INSERT INTO `customers` VALUES
+  (1, 'DEDOBBELER', 'Alan', '3351634541', 'aaaa', '$2y$11$c3e87b409637b1c293795eDgJkXhzZr0JgOEP4W9yTQNR24TMNESy',
+   '7 Chome 9-2 Quattro room 101, Shinjuku ku', 'Shinjuku', '16012', '2018-11-06 01:06:46'),
+  (4, 'bbbb', 'bbbb', 'bbbb', 'bbbb', '$2y$11$ac6e4286b0ebff3e13f3dONH1ii.vA0VTPefScckSF1460Y8h2UPO', 'bbbb', 'bbbb',
+   '0', '2018-11-07 23:39:43'),
+  (5, 'bbbb', 'bbbb', 'bbbb', 'cccc', '$2y$11$5746fd29033b8be7f9706ugyP3jXWO/NmHUFCqnfAW5rL2heujH.O', 'bbbb', 'bbbb',
+   '0', '2018-11-07 23:42:20'),
+  (6, 'qsc', 'qsc', '12345612', 'qsc@csq.csq', '$2y$11$269360175a9123da3d153uvWOgtvp4PKbsnF8/EjAR8SQF/XtjjDe', '', '',
+   '12345', '2018-11-09 00:47:36');
 /*!40000 ALTER TABLE `customers`
   ENABLE KEYS */;
 UNLOCK TABLES;
@@ -81,18 +123,18 @@ LOCK TABLES `meals` WRITE;
 /*!40000 ALTER TABLE `meals`
   DISABLE KEYS */;
 INSERT INTO `meals` VALUES
-  (1, 'Coca-Cola', 'Mmmm, le Coca-Cola avec 10 morceaux de sucres et tout plein de caféine !', 'coca.jpg', 72, 0.6, 3),
+  (1, 'Coca-Cola', 'Mmmm, le Coca-Cola avec 10 morceaux de sucres et tout plein de caféine !', 'coca.jpg', 1, 0.6, 3),
   (2, 'Bagel Thon',
    'Notre bagel est constitué d\'un pain moelleux avec des grains de sésame et du thon albacore, accompagné de feuilles de salade fraîche du jour  et d\'une sauce renversante :-)',
-   'bagel_thon.jpg', 18, 2.75, 5.5), (3, 'Bacon Cheeseburger',
-                                      'Ce délicieux cheeseburger contient un steak haché viande française de 150g ainsi que d\'un buns grillé juste comme il faut, le tout accompagné de frites fraîches maison !',
-                                      'bacon_cheeseburger.jpg', 14, 6, 12.5), (4, 'Carrot Cake',
-                                                                               'Le carrot cake maison ravira les plus gourmands et les puristes : tous les ingrédients sont naturels !\r\nÀ consommer sans modération',
-                                                                               'carrot_cake.jpg', 9, 3, 6.75),
+   'bagel_thon.jpg', -16, 2.75, 5.5), (3, 'Bacon Cheeseburger',
+                                       'Ce délicieux cheeseburger contient un steak haché viande française de 150g ainsi que d\'un buns grillé juste comme il faut, le tout accompagné de frites fraîches maison !',
+                                       'bacon_cheeseburger.jpg', 13, 6, 12.5), (4, 'Carrot Cake',
+                                                                                'Le carrot cake maison ravira les plus gourmands et les puristes : tous les ingrédients sont naturels !\r\nÀ consommer sans modération',
+                                                                                'carrot_cake.jpg', 9, 3, 6.75),
   (5, 'Donut Chocolat',
    'Les donuts sont fabriqués le matin même et sont recouvert d\'une délicieuse sauce au chocolat !',
    'chocolate_donut.jpg', 16, 3, 6.2),
-  (6, 'Dr. Pepper', 'Son goût sucré avec de l\'amande vous ravira !', 'drpepper.jpg', 53, 0.5, 2.9), (7, 'Milkshake',
+  (6, 'Dr. Pepper', 'Son goût sucré avec de l\'amande vous ravira !', 'drpepper.jpg', 12, 0.5, 2.9), (7, 'Milkshake',
                                                                                                       'Notre milkshake bien crémeux contient des morceaux d\'Oréos et est accompagné de crème chantilly et de smarties en guise de topping. Il éblouira vos papilles !',
                                                                                                       'milkshake.jpg',
                                                                                                       12, 2, 5.35);
@@ -171,4 +213,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-05 20:18:17
+-- Dump completed on 2018-11-14 23:08:53
