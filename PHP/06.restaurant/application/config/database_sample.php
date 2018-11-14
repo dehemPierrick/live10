6fb5229de197cc9914d736ce62d00a0e32b00764
Hoolay <hoolay@free.fr>
2018-11-14 22:05:12 +0900

    Restaurant - ajout des clés API stripe

--- PHP/06.restaurant/application/config/database_sample.php
+++ PHP/06.restaurant/application/config/database_sample.php
@@ -7,3 +7,6 @@
 $config['dsn'] = '';
 $config['password'] = '';
 $config['user'] = '';
+
+const STRIPE_PUBLIC_KEY = 'pk_test_FTIBMzyKXO5nAsJztle0cowV';
+const STRIPE_SECRET_KEY = 'sk_test_cBUOvMAIbfUloBEXb9wHWq4R';
